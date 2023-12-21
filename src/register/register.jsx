fe644761e74af97c0d969ad7f1dc8c0181d1c43f
BSC-04-20 <bsc-04-20@unima.ac.mw>
2023-12-21 11:38:18 +0200

    Added Register Template

---
+++ src/register/register.jsx
@@ -0,0 +1,14 @@
+function Register(){
+    
+    return(
+    <form>
+        <input type="text" required/>
+        <input type="text" required/>
+        <input type="email" required/>
+        <input type="number" required/>
+        <input type="password" required/>
+    </form>
+    )
+}
+
+export default Register();
