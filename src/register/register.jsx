c47d5177f28c5530ef43cce55548873f095fe899
BSC-04-20 <bsc-04-20@unima.ac.mw>
2023-12-21 12:21:09 +0200

    Saving Input to UseState

--- src/register/register.jsx
+++ src/register/register.jsx
@@ -1,21 +1,34 @@
-export function Register(){
-    
-    return(
-    <form>
-        <input type="text"  placeholder="firstname" required/><br/>
-        <input type="text" placeholder="lastname" required/><br/>
-        <input type="email" placeholder="email" required/><br/>
-        <input type="number" placeholder="phonenumber" required/><br/>
-        <input type="password" placeholder="password" required/><br/>
+import { useState } from "react";
 
-        <input type='radio' name="accountType" value="Student"/>
+export function Register(){
+    const [firstname, setFirstName] = useState();
+    const [lastname, setLastName] = useState();
+    const [email, setEmail] = useState();
+    const [phonenumber, setPhoneNumber] = useState();
+    const [password, setPassword] = useState();
+    const [accountType, setAccountType] = useState();
+    
+    async function handleRegister(event){
+        event.preventDefault()
+        console.log(accountType);
+    }
+
+    return(
+    <form onSubmit={handleRegister}>
+        <input type="text"  placeholder="firstname" onChange={(event) => setFirstName(event.target.value)} required/><br/>
+        <input type="text" placeholder="lastname" onChange={(event) => setLastName(event.target.value)} required/><br/>
+        <input type="email" placeholder="email" onChange={(event) => setEmail(event.target.value)} required/><br/>
+        <input type="number" placeholder="phonenumber" onChange={(event) => setPhoneNumber(event.target.value)} required/><br/>
+        <input type="password" placeholder="password" onChange={(event) => setPassword(event.target.value)} required/><br/>
+
+        <input type='radio' name="accountType" onChange={(event) => setAccountType(event.target.value)} value="Student" required/>
             <label>Student</label><br/>
-        <input type='radio' name="accountType" value="Landlord"/>
+        <input type='radio' name="accountType" onChange={(event) => setAccountType(event.target.value)} value="Landlord" required/>
             <label>Landlord</label><br/>
-        <input type='radio' name="accountType" value="Agent"/>
+        <input type='radio' name="accountType" onChange={(event) => setAccountType(event.target.value)} value="Agent" required/>
             <label>Agent</label><br/>
 
-        <input type="submit" value="Register"/>{/** Register Button */}
+        <input type="submit" value="Register"/>
     </form>
     )
 }
