d1214e5ac03ce6eb209587dffc3796cf1faa8487
BSC-04-20 <bsc-04-20@unima.ac.mw>
2023-12-21 11:49:38 +0200

    Login Page Template

---
+++ src/login/login.jsx
@@ -0,0 +1,9 @@
+export function Login(){
+    return(
+        <form>
+            <input type="email" placeholder="Email" required/><br/>
+            <input type="password" placeholder="Email" required/><br/>
+        </form>
+    )
+}
+
