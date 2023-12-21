15d1c27f5dd896d3695e114b2f3be0e49eaf942d
BSC-04-20 <bsc-04-20@unima.ac.mw>
2023-12-21 22:07:56 +0200

    Merged Branches

---
+++ src/viewHostels/viewHostels.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react"
+
+function ViewHostel(){
+    const [hostels, setHostels] = useState()
+    let hostelsArray = [];
+
+    
+    return(
+        <div>
+
+        </div>
+    )
+}
