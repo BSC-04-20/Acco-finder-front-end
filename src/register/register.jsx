cfba2da9ea39832c962581e21e7bc2f065de07c5
BSC-04-20 <bsc-04-20@unima.ac.mw>
2023-12-21 12:06:16 +0200

    Account Type Radios

--- src/register/register.jsx
+++ src/register/register.jsx
@@ -1,5 +1,5 @@
 export function Register(){
-
+    
     return(
     <form>
         <input type="text"  placeholder="firstname" required/><br/>
@@ -7,6 +7,15 @@ export function Register(){
         <input type="email" placeholder="email" required/><br/>
         <input type="number" placeholder="phonenumber" required/><br/>
         <input type="password" placeholder="password" required/><br/>
+
+        <input type='radio' name="accountType" value="Student"/>
+            <label>Student</label><br/>
+        <input type='radio' name="accountType" value="Landlord"/>
+            <label>Landlord</label><br/>
+        <input type='radio' name="accountType" value="Agent"/>
+            <label>Agent</label><br/>
+
+        <input type="submit" value="Register"/>{/** Register Button */}
     </form>
     )
 }
