c7672e3c6fd5530dfdf9f5a7c2488572c478cafb
BSC-04-20 <bsc-04-20@unima.ac.mw>
2023-12-21 11:50:13 +0200

    Changed Default to named Export

--- src/register/register.jsx
+++ src/register/register.jsx
@@ -1,14 +1,13 @@
-function Register(){
-    
+export function Register(){
+
     return(
     <form>
-        <input type="text" required/>
-        <input type="text" required/>
-        <input type="email" required/>
-        <input type="number" required/>
-        <input type="password" required/>
+        <input type="text"  placeholder="firstname" required/><br/>
+        <input type="text" placeholder="lastname" required/><br/>
+        <input type="email" placeholder="email" required/><br/>
+        <input type="number" placeholder="phonenumber" required/><br/>
+        <input type="password" placeholder="password" required/><br/>
     </form>
     )
 }
 
-export default Register();
